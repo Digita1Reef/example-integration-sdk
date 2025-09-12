de438042fb3fd4bda036e0432600030c36c167a7
Sidinei Silva <sidinei.silva02@gmail.com>
2025-09-12 11:33:47 -0300

    [HACK - SOMENTE DESENVOLVIMENTO] Alterando applicationId para funcionar com package da Siprocal
    Esse passo não é para ser feito, foi feito apenas para o desenvolvimento deste aplicativo por seu package não esta incluso na organization Siprocal

--- version-5/flutter/empty_example/android/app/build.gradle.kts
+++ version-5/flutter/empty_example/android/app/build.gradle.kts
@@ -21,7 +21,8 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.empty_example"
+        // applicationId = "com.example.empty_example"
+        applicationId = "com.siprocalsdk.appdemo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
